b978ce5a75560e6c3f997d52770bb56c6d175061
Damien Langhorn <dlanghorn11@gmail.com>
2025-06-07 00:24:11 -0400

    Deployment process changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Navbar from "./components/Navbar"
 import About from "./sections/About"
 import Contact from "./sections/Contact"
-import Hero from "./sections/hero"
+import Hero from "./sections/Hero"
 import Project from "./sections/Project"
 
 function App() {
