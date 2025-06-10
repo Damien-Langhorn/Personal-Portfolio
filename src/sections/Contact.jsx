1ccd520f86493b90fe31b020d65617c1b81ed94b
Damien Langhorn <dlanghorn11@gmail.com>
2025-06-09 21:15:25 -0400

    commit

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -121,25 +121,7 @@ const handleChange = (e) => {
   });
 };
 
-emailjs.init({
-  publicKey: 'VITE_APP_EMAILJS_PUBLIC_KEY',
-  serviceID: 'VITE_APP_EMAILJS_SERVICE_ID',
-  templateID: 'VITE_APP_EMAILJS_TEMPLATE_ID',
-  // Do not allow headless browsers
-  blockHeadless: true,
-  blockList: {
-    // Block the suspended emails
-    list: ['foo@emailjs.com', 'bar@emailjs.com'],
-    // The variable contains the email address
-    watchVariable: 'userEmail',
-  },
-  limitRate: {
-    // Set the limit rate for the application
-    id: 'app',
-    // Allow 1 request per 10s
-    throttle: 10000,
-  },
-});
+
 
 const handleSubmit = async (e) => {
   e.preventDefault();
